ff74ed55484a717b6510911ce0d28311e7d26d8a
Александр Корвешкин <cky@live.ru>
2022-02-11 23:09:18 +0400

    Правки под тесты

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -131,7 +131,7 @@ COUNT_POSTS = int(10)
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
-LOGOUT_REDIRECT_URL = 'posts:index'
+# LOGOUT_REDIRECT_URL = 'posts:index'
 
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
