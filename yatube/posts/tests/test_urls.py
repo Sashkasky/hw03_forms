560692c66a3f7f4e7fe56ae9babc2ffb30d4f5c9
Александр Корвешкин <cky@live.ru>
2022-04-08 20:20:22 +0400

    Доделал URL-тест

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -8,7 +8,7 @@ from ..models import Group, Post
 User = get_user_model()
 
 
-class TaskURLTests(TestCase):
+class PostsURLTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -32,6 +32,10 @@ class TaskURLTests(TestCase):
         self.authorized_client = Client()
         # Авторизуем пользователя
         self.authorized_client.force_login(self.user)
+        # Создаем третий клиент
+        self.author_client = Client()
+        # Авторизуем автора поста
+        self.author_client.force_login(self.user)
 
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
@@ -45,5 +49,35 @@ class TaskURLTests(TestCase):
         }
         for address, template in templates_url_names.items():
             with self.subTest(address=address):
-                response = self.authorized_client.get(address)
+                response = self.author_client.get(address)
                 self.assertTemplateUsed(response, template)
+
+    def test_pages_for_all(self):
+        """Проверяем доступ для неавторизованного клиента"""
+        guest_client = Client()
+        pages_for_all = [
+            '/',
+            f'/group/{self.group.slug}/',
+            f'/profile/{self.user.username}/',
+            f'/posts/{self.post.id}/'
+        ]
+        for page in pages_for_all:
+            with self.subTest(page=page):
+                response = guest_client.get(page)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_creation_page(self):
+        """Проверяем доступ авторизованного пользователя к созданию поста"""
+        response = self.authorized_client.get('/create/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_post_edit_only_for_author(self):
+        """Страница post_create доступна автору."""
+        response = self.author_client.get('/create/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_unexisting_page(self):
+        """Проверяем запрос к несуществующей странице"""
+        guest_client = Client()
+        response = guest_client.get('/unexisting_page/')
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
