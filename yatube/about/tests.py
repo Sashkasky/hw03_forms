6dd05f74ccdc58481811cacd65edf3653bcfc968
Александр Корвешкин <cky@live.ru>
2022-04-08 20:55:14 +0400

    Добавил URL-тест для About

--- yatube/about/tests.py
+++ yatube/about/tests.py
@@ -1,3 +1,26 @@
-# from django.test import TestCase
+# from http import HTTPStatus
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+
+
+User = get_user_model()
+
+
+class AboutURLTests(TestCase):
+    def setUp(self):
+        # Создаем неавторизованный клиент
+        self.guest_client = Client()
+        # Создаем пользователя
+        self.user = User.objects.create_user(username='HasNoName')
+
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        templates_url_names = {
+            '/about/author/': 'about/author.html',
+            '/about/tech/': 'about/tech.html',
+        }
+        for address, template in templates_url_names.items():
+            with self.subTest(address=address):
+                response = self.guest_client.get(address)
+                self.assertTemplateUsed(response, template)
