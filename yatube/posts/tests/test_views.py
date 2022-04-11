6c08b2380a686266b625271c366e57244a5a3694
Александр Корвешкин <cky@live.ru>
2022-04-11 21:38:32 +0400

    добавлен тест соответствия шаблонов views

---
+++ yatube/posts/tests/test_views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..models import Post, Group
+
+User = get_user_model()
+
+
+class PostsPagesTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Создадим запись в БД
+        cls.user = User.objects.create_user(username='SomeUser')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-group-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый текст',
+            author=cls.user
+        )
+
+    def setUp(self):
+        # Создаем авторизованный клиент
+        self.user = User.objects.create_user(username='NoNameUser')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    # Проверяем используемые шаблоны
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        # Собираем в словарь пары "имя_html_шаблона: reverse(name)"
+        templates_pages_names = {
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:group_list', kwargs={'slug': 'test-group-slug'}):
+                'posts/group_list.html',
+            reverse('posts:profile', kwargs={
+                'username': 'SomeUser'}):
+                'posts/profile.html',
+            reverse(
+                'posts:post_detail', kwargs={
+                    'post_id': PostsPagesTests.post.id
+                }
+            ):
+                'posts/post_detail.html',
+            reverse('posts:post_create'): 'posts/create_post.html',
+            reverse(
+                'posts:post_edit', kwargs={'post_id': PostsPagesTests.post.id}
+            ):
+                'posts/create_post.html'
+        }
+        for reverse_name, template in templates_pages_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
